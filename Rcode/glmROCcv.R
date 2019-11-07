c4f41f4bd99b78eaea7da3559620c61656d36772
Jason <prof.jason.m.graham@gmail.com>
2019-11-07 07:23:42 -0500

    Update glmROCcv.R
    update glmROCcv

--- Rcode/glmROCcv.R
+++ Rcode/glmROCcv.R
@@ -4,21 +4,18 @@ library(broom)
 library(caret)
 library(ROCR)
 
-train_ids <- createDataPartition(Default_to_fit$default_num,p=0.75,list=F)
+train_ids <- createDataPartition(Default$default,p=0.75,list=F)
 
-Default_train <- Default_to_fit[train_ids, ]
+Default_train <- Default[train_ids, ]
 
-Default_test <- Default_to_fit[-train_ids, ]
+Default_test <- Default[-train_ids, ]
 
 default_glm_mod = train(
   form = default ~ balance,
   data = Default_train,
   metric = "ROC",
-  trControl = trainControl(method = "repeatedcv", number = 10,repeats = 5,summaryFunction=twoClassSummary,,classProbs=TRUE),
+  trControl = trainControl(method = "repeatedcv", number = 10,repeats = 5,summaryFunction=twoClassSummary,classProbs=TRUE),
   method = "glm",
   family = "binomial")
 
-default_glm_mod$results
-
-
-
+default_glm_mod$resample
